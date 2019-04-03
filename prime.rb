9b4f99a059e33da6c4f61ba18b544a58fc53dd48
Jake L <jakelovitz@gmail.com>
2019-04-03 14:32:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(n)
  if n <= 3
    return n > 1
-  elsif n % 2 == 0 or n % 3 == 0
+  elsif n % 2 == 0 || n % 3 == 0
     puts "False1"
     return false
   end
   
   my_array = (5..n).to_a
-  my_array.each { |integer| if n % integer == 0 or n % (n +2) == 0; return false else; return true end }
+  my_array.each { |integer| if n % integer == 0 || n % (n +2) == 0; return false else; return true end }
 end
