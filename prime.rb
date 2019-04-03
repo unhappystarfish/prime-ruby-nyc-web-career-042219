e5dc72ff01d0deecdab86fc16542b0313cab0fa6
Jake L <jakelovitz@gmail.com>
2019-04-03 14:37:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(n)
+ if n <= 3
+   return n > 1
+  elsif n % 2 == 0 || n % 3 == 0
+    return false
+  end
+  
+  if (4...n).any? { |i| n % i == 0}
+    return false
+  else
+    return true
+  end
+end
